60a60e045d3b76a603d2db3eebd756c673c9cd5b
Nicomail <nicolas.maillet@free.fr>
2026-05-02 12:08:10 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "karaoke-box",
+  "version": "1.0.0",
+  "description": "Karaoké multi-micros WebRTC",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "engines": {
+    "node": ">=18.0.0"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2"
+  }
+}
