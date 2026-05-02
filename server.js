2541c7df93d511af075e8839873fab9624b5349d
Nicomail <nicolas.maillet@free.fr>
2026-05-02 12:44:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -14,18 +14,14 @@ const io = new Server(server, {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
 let hostSocket = null;
 const singers = new Map();
 
 io.on('connection', (socket) => {
-  console.log('Connexion:', socket.id);
-
   socket.on('register-host', () => {
     hostSocket = socket.id;
-    console.log('Box connectée:', socket.id);
     socket.emit('host-confirmed', { singerCount: singers.size });
     singers.forEach((data, id) => {
       socket.emit('singer-joined', { singerId: id, name: data.name, singerCount: singers.size });
@@ -33,4 +29,43 @@ io.on('connection', (socket) => {
   });
 
   socket.on('join-as-singer', ({ name }) => {
-    singers.set(sock
+    singers.set(socket.id, { name: name || 'Chanteur' });
+    if (hostSocket) {
+      io.to(hostSocket).emit('singer-joined', {
+        singerId: socket.id,
+        name: singers.get(socket.id).name,
+        singerCount: singers.size
+      });
+    }
+    socket.emit('joined-confirmed', { singerId: socket.id });
+  });
+
+  socket.on('offer', ({ offer, singerId }) => {
+    if (hostSocket) io.to(hostSocket).emit('offer', { offer, singerId });
+  });
+
+  socket.on('answer', ({ answer, singerId }) => {
+    io.to(singerId).emit('answer', { answer });
+  });
+
+  socket.on('ice-candidate', ({ candidate, targetId }) => {
+    io.to(targetId).emit('ice-candidate', { candidate, fromId: socket.id });
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.id === hostSocket) {
+      hostSocket = null;
+    } else if (singers.has(socket.id)) {
+      const name = singers.get(socket.id).name;
+      singers.delete(socket.id);
+      if (hostSocket) {
+        io.to(hostSocket).emit('singer-left', { singerId: socket.id, singerCount: singers.size });
+      }
+    }
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, '0.0.0.0', () => {
+  console.log('Serveur karaoke sur port ' + PORT);
+});
